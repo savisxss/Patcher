56868f2f938c55453f4c7cb5189acde3716becec
savis <106487343+savisxss@users.noreply.github.com>
2024-05-23 20:11:11 +0200

    Refactor settings.py to load configuration from external YAML file

--- settings.py
+++ settings.py
@@ -1,6 +1,13 @@
-SERVER_URL = 'https://YOURWEBSITE.com/'
-TARGET_FOLDER = 'patcher'
-FILELIST_URL = f'{SERVER_URL}patcher.txt'
-LOG_FILE = 'patcher.log'
-DOWNLOAD_SPEED_LIMIT = 100  # KB/s
-MULTITHREADING_THRESHOLD = 10485760  # bytes
+import yaml
+
+def load_config(config_file='config.yaml'):
+    with open(config_file, 'r') as file:
+        return yaml.safe_load(file)
+
+config = load_config()
+
+SERVER_URL = config['SERVER_URL']
+TARGET_FOLDER = config['TARGET_FOLDER']
+FILELIST_URL = config['FILELIST_URL']
+DOWNLOAD_SPEED_LIMIT = config['DOWNLOAD_SPEED_LIMIT']
+MULTITHREADING_THRESHOLD = config['MULTITHREADING_THRESHOLD']
